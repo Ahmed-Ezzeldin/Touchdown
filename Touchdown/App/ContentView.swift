8cd08514c317694cc33158f3807a16a6bb7a0a45
Ahmed <ahmed.ezzeldin.ibrahim@gmail.com>
2022-06-10 17:14:25 +0200

    Custom NavigationBar

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -11,8 +11,27 @@ struct ContentView: View {
     // MARK: ========================== Properites
     // MARK: ========================== Body
     var body: some View {
-     FooterView()
-            .padding(.horizontal)
+        ZStack {
+            VStack (spacing: 0){
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication
+                        .shared
+                        .connectedScenes
+                        .flatMap { ($0 as? UIWindowScene)?.windows ?? [] }
+                        .first { $0.isKeyWindow }?.safeAreaInsets.top)
+                    .background(.white)
+                    .shadow(color: Color.black.opacity(0.15) , radius: 5 , x: 0, y: 5 )
+                
+                Spacer()
+                
+                FooterView()
+                    .padding(.horizontal)
+            }
+            .background(colorBackground.ignoresSafeArea(.all ,edges: .all))
+        }
+        .ignoresSafeArea(.all ,edges: .top )
     }
 }
 
