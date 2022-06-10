fd544a5efbb604c8bbdfd361aab1822375f5ed17
Ahmed <ahmed.ezzeldin.ibrahim@gmail.com>
2022-06-10 14:56:10 +0200

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Codemaker on 10/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
