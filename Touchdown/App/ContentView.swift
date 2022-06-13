df327efa4c786617c11e63e41e64efefa3ed9f50
Ahmed <ahmed.ezzeldin.ibrahim@gmail.com>
2022-06-13 20:46:15 +0200

    BrandGridView

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -39,6 +39,9 @@ struct ContentView: View {
                         })
                         .padding(15)
                         
+                        TitleView(title: "Brands")
+                        BrandGridView()
+                        
                         FooterView()
                             .padding(.horizontal)
                     }
